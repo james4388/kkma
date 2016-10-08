bb49eb2db32b8f5de2912119df20e602f40d88ee
Trinh Hoang Nhu <trinhhoangnhu@gmail.com>
2016-10-08 00:02:21 -0700

    Add moer debug

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -2,22 +2,30 @@ import sys, os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
+def is_venv_python():
+    if len(sys.argv) > 0:
+        last_item = sys.argv[len(sys.argv)-1]
+        if last_item == 'venv-python':
+            return True
+    return False
+
 def _get_log():
     return file(os.path.join(BASE_DIR, 'tmp', 'log.txt'), 'a')
 log = _get_log()
 print >>log, "Running %s" % (sys.executable)
 print >>log, "sys.argv", sys.argv
+print >>log, "Is Venv Python", is_venv_python()
 
 sys.path.append(os.path.join(BASE_DIR, 'kkmadb'))  #You must add your project here or 500
 
 #Switch to new python
 #You may try to replace $HOME with your actual path
 PYTHON_PATH = os.path.join(BASE_DIR, 'env', 'bin', 'python')
-if sys.executable != PYTHON_PATH:
+if not is_venv_python():
     print >>log, "Detected wrong interpreter location, swapping to %s" % (PYTHON_PATH)
     log.flush()
     log.close()
-    os.execl(PYTHON_PATH, "python2.7.12", *sys.argv)
+    os.execl(PYTHON_PATH, "python2.7.12", *sys.argv, 'venv-python')
     
 log.flush()
 log.close()

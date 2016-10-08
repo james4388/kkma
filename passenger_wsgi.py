035ea07afd67c03c1b998a17b7f4cafb3e2e76a7
Trinh Hoang Nhu <trinhhoangnhu@gmail.com>
2016-10-08 00:13:33 -0700

    Remove debuger

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -10,26 +10,15 @@ def is_venv_python():
             return True
     return False
 
-def _get_log():
-    return file(os.path.join(BASE_DIR, 'tmp', 'log.txt'), 'a')
-log = _get_log()
-print >>log, "Running %s" % (sys.executable)
-print >>log, "sys.argv", sys.argv
-print >>log, "Is Venv Python", is_venv_python()
-
 sys.path.append(os.path.join(BASE_DIR, 'kkmadb'))  #You must add your project here or 500
 
 #Switch to new python
 #You may try to replace $HOME with your actual path
 PYTHON_PATH = os.path.join(BASE_DIR, 'env', 'bin', 'python')
 if not is_venv_python():
-    print >>log, "Detected wrong interpreter location, swapping to %s" % (PYTHON_PATH)
     log.flush()
     log.close()
     os.execl(PYTHON_PATH, "python2.7.12", *sys.argv + [VIRTUAL_ENV_PYTHON])
-    
-log.flush()
-log.close()
 
 sys.path.insert(0, os.path.join(BASE_DIR, 'env', 'bin'))
 sys.path.insert(0, os.path.join(

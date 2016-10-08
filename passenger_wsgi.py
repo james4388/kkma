92acc22e1c207e6f1de84b39a1e5dc8a8699380e
Trinh Hoang Nhu <trinhhoangnhu@gmail.com>
2016-10-08 00:05:03 -0700

    fix parameter

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,11 +1,12 @@
 import sys, os
 
+VIRTUAL_ENV_PYTHON = 'venv-python'  # Python > 2.7.6 dreamhost not return sys.executable
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 def is_venv_python():
     if len(sys.argv) > 0:
         last_item = sys.argv[len(sys.argv)-1]
-        if last_item == 'venv-python':
+        if last_item == VIRTUAL_ENV_PYTHON:
             return True
     return False
 
@@ -25,7 +26,7 @@ if not is_venv_python():
     print >>log, "Detected wrong interpreter location, swapping to %s" % (PYTHON_PATH)
     log.flush()
     log.close()
-    os.execl(PYTHON_PATH, "python2.7.12", *sys.argv, 'venv-python')
+    os.execl(PYTHON_PATH, "python2.7.12", *sys.argv, VIRTUAL_ENV_PYTHON)
     
 log.flush()
 log.close()

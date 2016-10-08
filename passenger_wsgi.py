2500d9debe9dc3485e9fe11cae611fa319d36c9b
Trinh Hoang Nhu <trinhhoangnhu@gmail.com>
2016-10-07 23:39:17 -0700

    Debug

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -2,13 +2,26 @@ import sys, os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
+def _get_log():
+    return file(os.path.join(BASE_DIR, 'tmp', 'log.txt'), 'a')
+log = _get_log()
+print >>log, "Running %s" % (sys.executable)
+
 sys.path.append(os.path.join(BASE_DIR, 'kkmadb'))  #You must add your project here or 500
 
 #Switch to new python
 #You may try to replace $HOME with your actual path
 PYTHON_PATH = os.path.join(BASE_DIR, 'env', 'bin', 'python')
 if sys.executable != PYTHON_PATH:
+    print >>log, "Detected wrong interpreter location, swapping to %s" % (PYTHON_PATH)
+    log.flush()
+    log.close()
     os.execl(PYTHON_PATH, "python2.7.12", *sys.argv)
+    
+log.flush()
+log.close()
+
+from paste.deploy import loadapp
 
 sys.path.insert(0, os.path.join(BASE_DIR, 'env', 'bin'))
 sys.path.insert(0, os.path.join(
@@ -19,5 +32,25 @@ sys.path.insert(0, os.path.join(
 ))
 
 os.environ['DJANGO_SETTINGS_MODULE'] = "kkmadb.settings"
+
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+# application = get_wsgi_application()
+
+def application(environ, start_response):
+    log = _get_log()
+    print >>log, "Application called:"
+    print >>log, "environ: %s" % str(environ)
+    results = []
+    try:
+        app = get_wsgi_application()
+        print >>log, "App loaded, attempting to run"
+        log.flush()
+        results = app(environ, start_response)
+        print >>log, "App executed successfully"
+    except Exception, inst:
+        print >>log, "Error: %s" % str(type(inst))
+        print >>log, inst.args
+        log.flush()
+    finally:
+        log.close()
+    return results
